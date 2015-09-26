90304a74d0bb0e30d7228839ce5dfa625359c8b4
pilyortueta <pilyortueta@10.33.193.21>
2015-09-26 12:16:34 -0500

    pdf1

---
+++ GravityCalculator/src/GravityCalculator.java
@@ -0,0 +1,25 @@
+
+public class GravityCalculator {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		// output the object´s position after 10 sec is 0.0m
+			double gravity = -9.81; // Earth's gravity in m/s^2
+
+			double initialVelocity = 0.0;
+
+			double fallingTime = 10.0;
+
+			double initialPosition = 0.0;
+
+			double finalPosition = 0.0;
+                   finalPosition = .5* (gravity * (fallingTime*fallingTime))+ (initialVelocity*fallingTime) + initialPosition;
+			 System.out.println("The object's position after " + fallingTime +
+
+			" seconds is " + finalPosition + " m.");
+             
+			}
+
+			}
+	
+
